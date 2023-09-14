608c3b1d350f4b6dd510520188fdb0e47cad389c
SAMUELLAHANNAH <remisaunders@googlemail.com>
2023-09-14 10:35:24 -0700

    2-mul.c

---
+++ alx-low_level_programming/0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiply 2 int
+ * @s: first int
+ * @b: second int
+ * Return: addition off the multiplicattion
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
