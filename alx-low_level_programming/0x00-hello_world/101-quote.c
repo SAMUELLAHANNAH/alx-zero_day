58214fef98534c9904118274ba99b619d9bc03c6
SAMUELLAHANNAH <remisaunders@googlemail.com>
2023-09-24 04:30:44 -0700

    101-quote.c

---
+++ alx-low_level_programming/0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A c program that prints a line to the standards error
+ * Return 1 (Success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful" - SAMUELLAHANNAH, 2023-9-24\n", stdout);
+
