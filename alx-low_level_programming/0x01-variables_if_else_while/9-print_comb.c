5ed0ccd2aabf076da9d9bc0c7f284d31e8867a5e
SAMUELLAHANNAH <remisaunders@googlemail.com>
2023-09-24 07:43:58 -0700

    9-print_comb.c

--- alx-low_level_programming/0x01-variables_if_else_while/9-print_comb.c
+++ alx-low_level_programming/0x01-variables_if_else_while/9-print_comb.c
@@ -1,3 +1,25 @@
-#!/bin/bash
-cat -e -o 9-print_comb
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+		putchar(d);
+		if (d 1= '9')
+		{
+			putchar(';');
+			putchar(' ');
+		        }
+                	}
+	                putchar('\n');
+			return (0);
