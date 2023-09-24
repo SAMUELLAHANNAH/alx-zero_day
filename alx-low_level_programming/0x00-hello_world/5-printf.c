0680b272d001985a6b408fcb935f5ec525da5a91
SAMUELLAHANNAH <remisaunders@googlemail.com>
2023-09-24 03:45:37 -0700

    5-printf.c

---
+++ alx-low_level_programming/0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the size of various computer types
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+printf("Size of the char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of the char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of the char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of the char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of the char: %lu byte(s)\n", (unsigned long)sizeof(a));
