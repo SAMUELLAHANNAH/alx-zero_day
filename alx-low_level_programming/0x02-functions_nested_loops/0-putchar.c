6c7240b15dcdfd00e7b73c622e4cce3374a307fa
SAMUELLAHANNAH <remisaunders@googlemail.com>
2023-09-24 07:34:33 -0700

    0-putchar

---
+++ alx-low_level_programming/0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Retunr: Always 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('n');
+	_putchar('r');
+	_putchar('\n');
+	retunr (0);
